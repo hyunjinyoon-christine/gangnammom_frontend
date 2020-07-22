3ea3a7d2b61841a1423ec1c6f5d00cc036d3df53
Yoon Hyunjin <christine.hyunjin.yoon@gmail.com>
2020-07-23 00:39:02 +0900

    show the data of posts

---
+++ gangnammom/src/Main.vue
@@ -0,0 +1,72 @@
+<template>
+  <div id="main" class="container">
+    <div class="main_list">
+        <div class="main_item">
+            <div class="contents" v-for="post in posts" :key="post.id">
+                <div class="thumbnail">
+                    <img v-bind:src="getThumbnail()"/>
+                </div>
+                <p>{{post.title}}</p>
+                <p>{{post.body}}</p>
+                <p>{{post.userId}}</p>
+            </div>            
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Main',
+  components: {
+
+  },
+  data (){
+    return {
+      thumbnails : [
+        'https://via.placeholder.com/150/771796',
+        'https://via.placeholder.com/150/92c952',
+        'https://via.placeholder.com/150/56a8c0'
+      ],
+      posts:[],
+      maxCount:8
+    }
+  },
+  mounted () {
+    this.requestEvents()   
+  },
+  methods: {
+    requestEvents: async function() {
+      this.posts = await this.$http.get('https://jsonplaceholder.typicode.com/posts')
+      this.posts = this.posts.data
+      console.log(this.posts)
+    },
+    getThumbnail() {
+      Math.random(this.thumbnails);
+    }
+
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import './styles/module';
+@media (min-width: 768px){
+    .container{
+        max-width: 1040px;
+    }
+    .main_list {
+        display: flex;
+        justify-content: center;
+        flex-wrap: wrap;
+    }
+    .main_item {
+        background-color: $gray-300;
+        padding: 1rem;
+        flex: 0 0 25%;
+        max-width: 25%;
+        border-radius: .5rem;
+    }
+    
+}
+</style>
--- gangnammom/src/main.js
+++ gangnammom/src/main.js
@@ -1,7 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Vuex from 'vuex'
+import axios from 'axios'
 
+
+// import 'expose-loader?$!expose-loader?jQuery!jquery'
+// import 'bootstrap'
+// import 'bootstrap/dist/css/bootstrap.min.css'
+
+Vue.use(Vuex)
 Vue.config.productionTip = false
+Vue.prototype.$http = axios
+
+
 
 new Vue({
   render: h => h(App),
