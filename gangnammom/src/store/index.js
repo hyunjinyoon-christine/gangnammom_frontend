ed97784646f84f1b80003ce528a7755677598f98
Yoon Hyunjin <christine.hyunjin.yoon@gmail.com>
2020-07-24 21:38:56 +0900

    create store

---
+++ gangnammom/src/store/index.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import axios from 'axios'
+
+Vue.use(Vuex)
+
+export const store = () => new Vuex.Store({
+    state: {        
+    },
+    mutations: {
+        
+    },
+    actions: {        
+    }
+})
+
+export default store
